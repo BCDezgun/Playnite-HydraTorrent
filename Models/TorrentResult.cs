495fe22a2978f0aab7853c10d08b772d98640da4
BCDezgun <nyx.mining@gmail.com>
2026-02-21 00:01:43 +0300

    feat: добавлено поле TorrentHash в TorrentResult для мониторинга прогресса

--- Models/TorrentResult.cs
+++ Models/TorrentResult.cs
@@ -11,5 +11,7 @@ namespace HydraTorrent.Models
         public string Year { get; set; }
         public string CoverUrl { get; set; }    // для будущей обложки
         public string PageUrl { get; set; }     // ссылка на страницу торрента
+
+        public string TorrentHash { get; set; }   // InfoHash торрента из qBittorrent
     }
 }
